465a81ecc68fe7757d262448a3942a90b9407cb4
Connor Ferster <connorferster@gmail.com>
2025-09-10 13:45:40 -0700

    Bump millrun version

--- structural_starterkit/__init__.py
+++ structural_starterkit/__init__.py
@@ -2,4 +2,4 @@
 A module to designed to perform package installations, and verification of install,
 for students enrolled in Structural Python courses.
 """
-__version__ = "0.4.0"
+__version__ = "0.4.1"
